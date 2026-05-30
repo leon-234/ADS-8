0abc745de7e6a8a128571d996642b8b658759f25
leon-234 <rust824@mail.ru>
2026-05-30 16:03:55 +0300

    Ильин. 25-ИСТ-1-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
-#include <string>
-
-void makeTree(BST<std::string>& tree, const char* filename);
-void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;

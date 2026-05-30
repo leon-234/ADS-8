035f008d9dde65781d1f7b0c5bfb0e14048246fe
leon-234 <rust824@mail.ru>
2026-05-30 15:44:41 +0300

    Ильин. 25-ИСТ-1-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,45 @@
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
+#include <string>
 #include  <cstdlib>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file)
+        return;
+    std::string word;
+    while (!file.eof()) {
+        int ch = file.get();
+        if (std::isalpha(ch) && ch < 128) {
+            word += std::tolower(ch);
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+    if (!word.empty())
+        tree.insert(word);
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+    auto data = tree.getData();
+    std::sort(data.begin(), data.end(),
+        [](const auto& a, const auto& b) {
+            if (a.second != b.second)
+                return a.second > b.second;
+
+            return a.first < b.first;
+        });
+    std::ofstream out("result/freq.txt");
+    for (const auto& p : data) {
+        std::cout << p.first << " " << p.second << std::endl;
+        if (out)
+            out << p.first << " " << p.second << '\n';
+    }
+    out.close();
 }
